1a24c7e4de6f74bbdd157fa1502affb42c3fcace
Radu Girlea <radu.girlea@yahoo.com>
2024-06-28 14:13:06 +0300

    add models for User, Workout, Exercise and Exercise Log

---
+++ SmartWorkout/Entities/Workout.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Identity.Client;
+
+namespace SmartWorkout.Entities
+{
+    public class Workout
+    {
+        public int Id { get; set; }
+        
+        public string Name { get; set; }
+        
+        public DateTime Date { get; set; }
+        
+        public int UserId { get; set; }
+        
+        public User User { get; set; }
+
+        public ICollection<ExerciseLog> ExerciseLogs { get; set; }
+    }
+}
