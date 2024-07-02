849827affe1d28c2495118ddb0de14e955e090e1
Radu Girlea <radu.girlea@yahoo.com>
2024-07-03 00:09:43 +0300

    add edit / add form for User and Exercise

---
+++ SmartWorkout/Components/Pages/AddOrEditExercises.razor.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+using SmartWorkout.Entities;
+using SmartWorkout.Repositories.Implementations;
+using SmartWorkout.Repositories.Interfaces;
+
+namespace SmartWorkout.Components.Pages
+{
+    public partial class AddOrEditExercises : ComponentBase
+    {
+        [Inject]
+        public IExerciseRepository exerciseRepository { get; set; }
+
+        [SupplyParameterFromForm]
+        public Exercise Exercise { get; set; } = new Exercise();
+
+        public async Task AddExerciseAsync()
+        {
+            await exerciseRepository.AddAsync(Exercise);
+        }
+    }
+}
